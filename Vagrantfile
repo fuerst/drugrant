45abe8d4164a5603465d9c2d3895743cb7c45b0f
Bernhard Fürst <bernhard.fuerst@fuerstnet.de>
2014-12-05 19:00:20 +0100

    Initial commit of project files

---
+++ Vagrantfile
@@ -0,0 +1,77 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+# Default Konfiguration - für lokale Anpassungen nicht hier ändern.
+# Anpassungen dieser Variablen sind in einer Datei "Vagrantfile.local" möglich.
+$my_name             = "drugrant"
+$my_hostname         = "#{$my_name}.dev.local" unless defined? $my_hostname
+$my_ip               = "192.168.50.2"
+$my_box              = "drugrant" unless defined? $my_box
+$my_box_url          = "http://fuerstnet.de/vagrant_boxes/#{$my_box}-virtualbox.box" unless defined? $my_box_url
+$my_box_url_vmware   = "http://fuerstnet.de/vagrant_boxes/#{$my_box}-vmware-fusion.box" unless defined? $my_box_url_vmware
+$my_memory           = "512" unless defined? $my_memory
+$my_nfs              = true unless defined? $my_nfs
+$my_gui              = false unless defined? $my_gui
+$my_noproxy          = "#{$my_hostname}" unless defined? $my_noproxy
+$my_halt_timeout     = 30 unless defined? $my_halt_timeout
+$my_cpus             = 1 unless defined? $my_cpus
+$my_sql_host_port    = 3306 unless defined? $my_sql_host_port
+
+# Lokale Anpassungen laden.
+# Beispiel für eine Vagrantfile.local, mit der eine weitere VM im Host gestartet
+# werden kann, um in einem anderen Branch arbeiten zu können:
+#   $my_name     = "my_drugrant"
+#   $my_ip   = "192.168.50.3"
+#   $my_cpus = 2
+#   $my_sql_host_port = 3307
+
+load 'Vagrantfile.local' if File.exists? 'Vagrantfile.local'
+
+Vagrant.configure("2") do |config|
+    # SSH-Key des Host-Users innerhalb der VM nutzen
+    config.ssh.forward_agent = true
+
+    # Virtualbox Folder Sharing ist unter Mac/Linux performanter mit NFS.
+    # Mit VMware Fusion 6.0.2. (HGFS) wurden Dateien unvollständig übertragen,
+    # siehe #26 https://communities.vmware.com/thread/438804
+    # bzw. https://communities.vmware.com/thread/462747
+    config.vm.synced_folder ".", "/vagrant", type: "nfs"
+
+    config.vm.box = "#{$my_box}"
+    config.vm.box_url = "#{$my_box_url}"
+
+    # Statische IP zum Zugriff auf die VM. Kann in eigener /etc/hosts
+    # folgendermaßen verwendet werden (Beispiel):
+    # 192.168.50.2 drugrant.dev.local
+    config.vm.network :private_network, ip: "#{$my_ip}"
+
+    config.vm.hostname = "#{$my_hostname}"
+
+    config.vm.graceful_halt_timeout = $my_halt_timeout
+
+    config.vm.network "forwarded_port", guest: 3306, host: "#{$my_sql_host_port}", protocol: 'tcp'
+
+    config.vm.provider :virtualbox do |vb, override|
+        vb.gui = $my_gui
+        vb.customize [
+            "modifyvm", :id,
+            "--memory", "#{$my_memory}",
+            "--cpus", "#{$my_cpus}"
+        ]
+    end
+
+    config.vm.provider :vmware_fusion do |vb, override|
+        vb.gui = $my_gui
+        vb.vmx["memsize"] = "#{$my_memory}"
+        vb.vmx["numvcpus"] = "#{$my_cpus}"
+        override.vm.box_url = "#{$my_box_url_vmware}"
+    end
+
+    config.vm.provision :shell do |shell|
+        shell.path = "provisioning.sh"
+        shell.args = "#{$my_ip} #{$my_hostname} #{ENV['PROVISION_ARGS']}"
+    end
+end
---
+++ provisioning.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env bash
+#
+# Provisioning für Vagrant
+# Siehe http://docs.vagrantup.com/v2/provisioning/shell.html
+#
+# Wichtig: Dieses Skript kann mehr als einmal laufen. Alle Aktionen müssen
+# sich entsprechend darauf einstellen.
+#
+# Hinweis: Lange Befehle nicht per Backslash in mehrere Zeilen auftrennen.
+# Vagrant stolpert dabei.
+#
+# Über die ENV-Variable PROVISION_ARGS können in diesem Skript Dinge getriggert
+# werden. Dazu wird im Vagrantfile PROVISION_ARGS als Argument an das Skript
+# übergeben. Hier werden die leerzeichen-getrennten Werte einzeln getestet.
+# Bash hat je nach Version unterschiedlich Syntax für Regex, daher wird per
+# "echo|grep" Kombination getestet.
+
+# Fehler "stdin ist no tty" beim Provisioning verhindern
+sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+
+# Debian-Pakete upgraden
+echo "* Debian-Pakete upgraden..."
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get --yes dist-upgrade
+
+# Memcached konfigurieren
+echo "* Memcached konfigurieren..."
+sed -i 's/^-m 64$/-m 128/g' /etc/memcached.conf
+service memcached restart
+
+# Apache konfigurieren
+echo "* Apache konfigurieren..."
+a2enmod rewrite
+
+# IP und Hostname werden in Vagrantfile als Argument übergeben
+command echo "<VirtualHost ${1}:80>
+    ServerName ${2}
+
+    DocumentRoot /vagrant/drupal
+
+    ErrorLog  /var/log/apache2/drupal_error_log
+    CustomLog /var/log/apache2/drupal_access_log combined
+
+    # don't loose time with IP address lookups
+    HostnameLookups Off
+
+    # needed for named virtual hosts
+    UseCanonicalName Off
+
+    # configures the footer on server-generated documents
+    ServerSignature Off
+
+    <Directory "/vagrant/drupal">
+      Options FollowSymLinks
+      DirectoryIndex index.php
+      AllowOverride All
+      Require all granted
+    </Directory>
+
+</VirtualHost>" \
+    > '/etc/apache2/sites-available/drupal.conf'
+a2ensite drupal.conf
+
+service apache2 restart
+
+# MySQL konfigurieren
+echo "* MySQL konfigurieren..."
+
+command echo "
+[mysqld]
+innodb_doublewrite = 0
+innodb_flush_log_at_trx_commit = 0
+innodb_flush_method = O_DIRECT
+innodb_log_buffer_size = 4M
+innodb_buffer_pool_size = 128M
+innodb_file_per_table = 1
+
+default-storage-engine = InnoDB
+query_cache_limit = 128K
+query_cache_size = 128M
+sort_buffer_size = 16M
+bulk_insert_buffer_size = 4M
+tmp_table_size = 32M
+max_heap_table_size = 32M
+key_buffer_size = 64M
+max_allowed_packet = 16M
+thread_stack = 256K
+thread_cache_size = 8
+max-connections = 32
+table-cache = 256
+thread-concurrency = 2
+general_log=0
+general_log_file=/vagrant/mysqlquery.log
+" \
+    > '/etc/mysql/conf.d/drupal.cnf'
+
+# Binary Log ausschalten - produziert unnötigen Overhead
+sed -i -e 's/log_bin/#log_bin/' -e 's/expire_logs_days/#expire_logs_days/' /etc/mysql/my.cnf
+# MySQL von außen per TCP erreichbar
+sed -i -e 's/^bind-address   = 127.0.0.1$/bind-address    = 0.0.0.0/' /etc/mysql/my.cnf
+service mysql restart
+
+# PHP konfigurieren
+echo "* PHP konfigurieren..."
+command echo "
+memory_limit = 256M
+
+opcache.enable=On
+opcache.memory_consumption=128
+opcache.interned_strings_buffer=8
+opcache.max_accelerated_files=4000
+opcache.revalidate_freq=0
+opcache.fast_shutdown=1
+opcache.enable_cli=1
+opcache.save_comments=1
+
+
+error_log = /var/log/apache2/drupal_error_log
+display_errors = On
+display_startup_errors = On
+
+post_max_size = 25M
+upload_max_filesize = 25M
+
+date.timezone = Europe/Berlin
+
+; Debugging via Netbeans auf dem Host
+xdebug.default_enable = 1
+xdebug.remote_enable = 1
+xdebug.remote_handler = dbgp
+xdebug.remote_connect_back = 1
+xdebug.remote_port = 9000
+xdebug.remote_autostart = 0
+xdebug.idekey = netbeans-xdebug
+;xdebug.remote_log = /tmp/xdebug.log
+" \
+    > '/etc/php5/apache2/conf.d/drupal.ini'
+cp /etc/php5/apache2/conf.d/drupal.ini /etc/php5/cli/conf.d/drupal.ini
+service apache2 restart
+
+# Install Drupal 7: PROVISION_ARGS="install_drupal7" vagrant provision
+if [[ $(echo "$@" | grep -e '\binstall_drupal7\b') ]]
+then
+  echo "* Install Drupal 7..."
+  mysqladmin drop -f drupal
+  mysqladmin create drupal
+  chmod 755 /vagrant/drupal/sites/default/
+  rm -rf /vagrant/drupal
+  mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON drupal.* TO 'vagrant'@'192.168.50.1' IDENTIFIED BY 'vagrant'"
+  mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON drupal.* TO 'vagrant'@'localhost' IDENTIFIED BY 'vagrant';"
+  /home/vagrant/.composer/vendor/bin/drush dl drupal-7 --drupal-project-rename=drupal --destination=/vagrant
+  cd /vagrant/drupal
+  /home/vagrant/.composer/vendor/bin/drush site-install standard --site-name=Drugrant--account-name=admin --account-pass=admin --db-url=mysql://vagrant:vagrant@localhost/drupal -y
+fi
+
+# Install Drupal 7: PROVISION_ARGS="install_drupal7" vagrant provision
+if [[ $(echo "$@" | grep -e '\binstall_drupal8\b') ]]
+then
+  echo "* Install Drupal 8..."
+  mysqladmin drop -f drupal
+  mysqladmin create drupal
+  chmod 755 /vagrant/drupal/sites/default/
+  rm -rf /vagrant/drupal
+  mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON drupal.* TO 'vagrant'@'192.168.50.1' IDENTIFIED BY 'vagrant'"
+  mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON drupal.* TO 'vagrant'@'localhost' IDENTIFIED BY 'vagrant';"
+  /home/vagrant/.composer/vendor/bin/drush dl drupal-8 --drupal-project-rename=drupal --destination=/vagrant
+  cd /vagrant/drupal
+  /home/vagrant/.composer/vendor/bin/drush site-install standard --site-name=Drugrant --account-name=admin --account-pass=admin --db-url=mysql://vagrant:vagrant@localhost/drupal -y
+fi
